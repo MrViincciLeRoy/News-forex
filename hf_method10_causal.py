a0d2ef116ca5cef2c4f1222d2528236078d424d6
Mr Viincci Le Roy <60152334+MrViincciLeRoy@users.noreply.github.com>
2026-02-05 16:24:06 +0200

    Update hf_method10_causal.py

--- hf_method10_causal.py
+++ hf_method10_causal.py
@@ -16,7 +16,7 @@ class HFCausalExplainer:
     Explains WHY correlations exist and WHAT drives patterns
     """
     
-    def __init__(self, model_name: str = "gpt2"):
+    def __init__(self, model_name: str = "FinancialSupport/NanoGPT"):
         self.model_name = model_name
         self.pipeline = None
         self.explanation_cache = {}
